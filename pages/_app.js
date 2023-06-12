33b469edd6e7479d05a162da4d794753caf894c0
Harmoniousmoss <syaefulbahri@protonmail.com>
2023-06-12 08:56:39 +0700

    Initial Deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/js_composer.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
